1eb4e8b9554f593389100a13283704127f1c1aa1
bsEmo <37816440+bsEmo@users.noreply.github.com>
2019-07-23 13:55:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,85 +1,87 @@
 const inquirer = require('inquirer');
 const sqlanywhere = require('sqlanywhere');
 const fs = require('fs');
-const dotenv = require('dotenv').config();
 
 (async () => {
   console.log(`************************************`);
   console.log(`\tBEGIN`);
   console.log(`************************************\n`);
 
-  if (dotenv.error) {
-    console.error(dotenv.error);
-    return;
-  }
+  const files = fs.readdirSync(__dirname + "//script_to_run//");
 
-  console.info(dotenv);
-
-  const answer = await inquirer
+  const fileAnswer = await inquirer
     .prompt([{
-      type: 'confirm',
-      message: `Proper configuration??`,
-      name: "config",
-      default: false
+      type: 'list',
+      message: `Which file?`,
+      name: "fileName",
+      choices: files
     }]);
 
-  if (!answer.config) {
+  if (!fileAnswer.fileName) {
     return;
   }
 
-  console.info("\n");
-  console.info(fs.readdirSync(__dirname + "//script_to_run//"));
-
-  const answer2 = await inquirer
-    .prompt([{
-      type: 'confirm',
-      message: `File in there??`,
-      name: "files",
-      default: false
-    }]);
-
-  if (!answer2.files) {
-    return;
-  }
-
-  const db = await getDbInstance();
-
-  const fileContents = fs.readFileSync(__dirname + "//script_to_run//" + dotenv.FILE_NAME, 'utf8');
-
-  if (dotenv.ACROSS_SCHEMAS) {
-    const schemaNames = await getSchemaNames(db, dotenv.PROC_NAME);
-
-    // MANUAL SCHEMA NAMES
-    // const schemaNames = [
-    //   "CIGActgS7",
-    //   "CIGActgS8"
-    // ];
-
-    console.log(`Found SCHEMAS:\n${schemaNames.map(s => `\t${s}\n`)}\n`);
-
-    for (let index = 0; index < schemaNames.length; index++) {
-      let mutableFileContents = String(fileContents);
-      const schemaName = schemaNames[index];
-
-      console.log(`Starting execution on: ${schemaName} @${new Date()}`);
-      mutableFileContents = mutableFileContents.replace(/cigactgs0/gi, schemaName);
-
-      let statements = mutableFileContents.split(/\bgo\b/ig);
-
-      for (let sIndex = 0; sIndex < statements.length; sIndex++) {
-        const statement = statements[sIndex];
-
-        if (statement.trim().length > 0) {
-          await db.exec(statement);
-        }
+  const manyAnswers = await inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "dbHost",
+        message: "Please enter DB Host (format: HOST:PORT)"
+      },
+      {
+        type: "input",
+        name: "dbName",
+        message: "Please enter DB Name"
+      },
+      {
+        type: "input",
+        name: "username",
+        message: "Please enter DB Account Username"
+      },
+      {
+        type: "password",
+        name: "password",
+        message: "Please enter DB Account Password"
       }
+    ]);
 
-      console.log(`FINISHED execution on: ${schemaName} @${new Date()}\n`);
+  const db = await getDbInstance(manyAnswers.username, manyAnswers.password, manyAnswers.dbHost, manyAnswers.dbName);
+
+  const schemaNames = await getSchemaNames(db, "spGlobRpt_AcctPLRollUp_Extract");
+
+  console.log(`Found SCHEMAS:\n${schemaNames.map(s => `\t${s}\n`)}\n`);
+  const schemaContinueAnswer = await inquirer
+    .prompt([{
+      type: 'confirm',
+      message: "Continue?",
+      name: "continue",
+      default: false
+    }]);
+
+  if (!schemaContinueAnswer.continue) {
+    return;
+  }
+
+  const fileContents = fs.readFileSync(__dirname + "//script_to_run//" + fileAnswer.fileName, 'utf8');
+
+  for (let index = 0; index < schemaNames.length; index++) {
+    let mutableFileContents = String(fileContents);
+    const schemaName = schemaNames[index];
+
+    console.log(`Starting execution on: ${schemaName} @${new Date()}`);
+    mutableFileContents = mutableFileContents.replace(/cigactgs0/gi, schemaName);
+
+    let statements = mutableFileContents.split(/\bgo\b/ig);
+
+    for (let sIndex = 0; sIndex < statements.length; sIndex++) {
+      const statement = statements[sIndex];
+
+      if (statement.trim().length > 0) {
+        await db.exec(statement);
+      }
     }
-  } else {
-    console.log(`Starting execution`);
-    executeQuery(fileContents);
-    console.log(`FINISHED execution`);
+
+    console.log(`FINISHED execution on: ${schemaName} @${new Date()}\n`);
   }
 
   db.disconnect();
@@ -88,14 +90,14 @@ const dotenv = require('dotenv').config();
   console.log(`************************************`);
 })();
 
-function getDbInstance() {
+function getDbInstance(username, password, host, dbName) {
   const connection = sqlanywhere.createConnection();
 
   const connectionParams = {
-    Host: dotenv.DB_HOST,
-    DatabaseName: dotenv.DB_NAME,
-    Userid: dotenv.UID,
-    Password: dotenv.PW
+    Host: host,
+    DatabaseName: dbName,
+    Userid: username,
+    Password: password
   };
 
   connection.connect(connectionParams);
